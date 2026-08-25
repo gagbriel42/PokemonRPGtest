9272c0cc9248b4b3766bc1e40e5c28cd5a18ee90
gagbriel42 <gagbriel42@gmail.com>
2026-08-25 11:41:36 +0200

    style: scope battle UI for HGSS presentation

--- apps/web/src/battle/AdvancedBattleView.jsx
+++ apps/web/src/battle/AdvancedBattleView.jsx
@@ -2,6 +2,6 @@ import React,{useState}from'react';
 import BattleSystemV2,{GmBattleSystemV2}from'./BattleSystemV2.jsx';
 import PokemonDex from'./PokemonDex.jsx';
 import'./hgss-battle-ui.css';
-function Shell({gm=false,onExit}){const[open,setOpen]=useState(false);return <><div className="hgss-dex-launcher"><button type="button" onClick={()=>setOpen(true)}>POKÉDEX NATIONAL</button></div>{gm?<GmBattleSystemV2 onExit={onExit}/>:<BattleSystemV2 onExit={onExit}/>}<PokemonDex open={open} onClose={()=>setOpen(false)} onSelect={p=>{localStorage.setItem('pokemon-jdr-dex-last',JSON.stringify(p));setOpen(false);window.dispatchEvent(new CustomEvent('pokemon-jdr-dex-selected',{detail:p}))}}/></>}
+function Shell({gm=false,onExit}){const[open,setOpen]=useState(false);return <div className="hgss-battle-shell"><div className="hgss-dex-launcher"><button type="button" onClick={()=>setOpen(true)}>POKÉDEX NATIONAL</button></div>{gm?<GmBattleSystemV2 onExit={onExit}/>:<BattleSystemV2 onExit={onExit}/>}<PokemonDex open={open} onClose={()=>setOpen(false)} onSelect={p=>{localStorage.setItem('pokemon-jdr-dex-last',JSON.stringify(p));setOpen(false);window.dispatchEvent(new CustomEvent('pokemon-jdr-dex-selected',{detail:p}))}}/></div>}
 export default function AdvancedBattleView(props){return <Shell {...props}/>}
 export function GmAdvancedBattleView(props){return <Shell {...props} gm/>}
