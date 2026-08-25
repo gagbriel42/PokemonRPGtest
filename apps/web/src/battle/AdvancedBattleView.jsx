88e8e6c6f629b78359ae8146659ac020cbc84cd2
gagbriel42 <gagbriel42@gmail.com>
2026-08-25 11:54:44 +0200

    fix: remove standalone Pokedex and use integrated Pokemon selection

--- apps/web/src/battle/AdvancedBattleView.jsx
+++ apps/web/src/battle/AdvancedBattleView.jsx
@@ -1,7 +1,5 @@
-import React,{useState}from'react';
+import React from'react';
 import BattleSystemV2,{GmBattleSystemV2}from'./BattleSystemV2.jsx';
-import PokemonDex from'./PokemonDex.jsx';
 import'./hgss-battle-ui.css';
-function Shell({gm=false,onExit}){const[open,setOpen]=useState(false);return <div className="hgss-battle-shell"><div className="hgss-dex-launcher"><button type="button" onClick={()=>setOpen(true)}>POKÉDEX NATIONAL</button></div>{gm?<GmBattleSystemV2 onExit={onExit}/>:<BattleSystemV2 onExit={onExit}/>}<PokemonDex open={open} onClose={()=>setOpen(false)} onSelect={p=>{localStorage.setItem('pokemon-jdr-dex-last',JSON.stringify(p));setOpen(false);window.dispatchEvent(new CustomEvent('pokemon-jdr-dex-selected',{detail:p}))}}/></div>}
-export default function AdvancedBattleView(props){return <Shell {...props}/>}
-export function GmAdvancedBattleView(props){return <Shell {...props} gm/>}
+export default function AdvancedBattleView({onExit}){return <div className="hgss-battle-shell"><BattleSystemV2 onExit={onExit}/></div>}
+export function GmAdvancedBattleView({onExit}){return <div className="hgss-battle-shell"><GmBattleSystemV2 onExit={onExit}/></div>}
