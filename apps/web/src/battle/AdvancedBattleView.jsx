759674d04291442ade0594117545c56ec05c0dd8
gagbriel42 <gagbriel42@gmail.com>
2026-08-26 18:30:50 +0200

    Load battle layout fixes

--- apps/web/src/battle/AdvancedBattleView.jsx
+++ apps/web/src/battle/AdvancedBattleView.jsx
@@ -1,5 +1,6 @@
 import React from'react';
 import BattleSystemV2,{GmBattleSystemV2}from'./BattleSystemV2.jsx';
 import'./hgss-battle-ui.css';
+import'./hgss-battle-fixes.css';
 export default function AdvancedBattleView({onExit}){return <div className="hgss-battle-shell"><BattleSystemV2 onExit={onExit}/></div>}
 export function GmAdvancedBattleView({onExit}){return <div className="hgss-battle-shell"><GmBattleSystemV2 onExit={onExit}/></div>}
