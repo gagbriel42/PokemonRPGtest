ec3f83734987e443f6d9b7efb74d7a7251c8ddcd
gagbriel42 <gagbriel42@gmail.com>
2026-08-27 13:37:38 +0200

    Branche le combat principal sur le système persistant avec équipes et objets

--- apps/web/src/battle/AdvancedBattleView.jsx
+++ apps/web/src/battle/AdvancedBattleView.jsx
@@ -1,4 +1,4 @@
 import React from'react';
-import BattleViewFixed,{GmBattleViewFixed}from'./BattleViewFixed.jsx';
-export default function AdvancedBattleView({onExit}){return <BattleViewFixed onExit={onExit}/>}
-export function GmAdvancedBattleView({onExit}){return <GmBattleViewFixed onExit={onExit}/>}
+import BattlePersistent,{GmBattlePersistent}from'./BattlePersistent.jsx';
+export default function AdvancedBattleView({onExit}){return <BattlePersistent onExit={onExit}/>}
+export function GmAdvancedBattleView({onExit}){return <GmBattlePersistent onExit={onExit}/>}
